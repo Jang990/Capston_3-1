047dd4e9cab2268bbe35d28ef881d443dc1ebb9a
Jang_Desktop <sdsd090811@gmail.com>
2022-08-10 10:55:07 +0900

    Add 오라클에서 MySQL설정으로 바꿈

--- src/test/java/com/esummary/EsummaryApplicationTests.java
+++ src/test/java/com/esummary/EsummaryApplicationTests.java
@@ -1,29 +1,42 @@
 package com.esummary;
 
 import java.util.List;
+import java.util.Properties;
 
+import javax.sql.DataSource;
 import javax.transaction.Transactional;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import com.esummary.elearning.entity.subject.SubjectInfo;
 import com.esummary.elearning.entity.subject.SubjectLectureWeekInfo;
 import com.esummary.elearning.service.subject.util.db.SubjectUtil_DB;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 
 @SpringBootTest
 class EsummaryApplicationTests {
 	
-	@Autowired
-	private SubjectUtil_DB testSubjectDB;
 	
 	@Test
-	@Transactional
-	void contextLoads() {
-		SubjectInfo subject = testSubjectDB.getSubjectAllDetails("202214043C4846");
-		System.out.println(subject);
+	public void hikariConfig() {
+		HikariConfig h = new HikariConfig();
+		h.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		h.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:4306/InhaTcES");
+		h.setUsername("root");
+		h.setPassword("1234");
+		DataSource dataSource = new HikariDataSource(h);
+		System.out.println("===============>" + dataSource.toString());
 	}
-
+	
+	
 }
