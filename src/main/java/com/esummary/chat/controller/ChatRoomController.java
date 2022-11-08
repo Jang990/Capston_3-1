d2348daf5f8b92101036e1679f66f18c56990e82
Jang_Desktop <sdsd090811@gmail.com>
2022-11-08 18:55:26 +0900

    Add 백엔드 roomId별로 채팅을 처리하도록 변경

--- src/main/java/com/esummary/chat/controller/ChatRoomController.java
+++ src/main/java/com/esummary/chat/controller/ChatRoomController.java
@@ -1,10 +1,10 @@
 package com.esummary.chat.controller;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.MessageMapping; 
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.HtmlUtils;
 
 import com.esummary.chat.model.ChatMessageDTO;
 import com.esummary.chat.model.ChatMsgForTestDTO;
@@ -14,14 +14,19 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class ChatRoomController {
+	private final SimpMessageSendingOperations sendingOperations;
 	
 	/*
 	 	/receive로 메시지가 들어오면
-		/topic/chat을 구독한 사용자들에게 메시지를 뿌려준다
+		"/topic/chat/room/" + message.getRoomId()을 구독한 사용자들에게 
+		ChatMsgForTestDTO 객체를 JSON형태로 뿌려준다
 	 */
 	@MessageMapping("/receive")
-	@SendTo("/topic/chat")
-	public ChatMsgForTestDTO test(ChatMessageDTO message) { 
-		return new ChatMsgForTestDTO(message.getSender(), message.getMessage());
+//	@SendTo("/topic/chat")
+	public void test(ChatMessageDTO message) { 
+		sendingOperations.convertAndSend(
+				"/topic/chat/room/" + message.getRoomId(), 
+				new ChatMsgForTestDTO(message.getSender(), message.getMessage())
+			);
 	}
 }
