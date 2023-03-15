8f3ea29130280e3d3dcd22b39db8105a83fd5f14
Jang_Desktop <sdsd090811@gmail.com>
2023-03-15 21:05:17 +0900

    Test: InhaTc 관련 테스트 코드 틀만 추가

---
+++ src/test/java/com/esummary/crawler/InhaTcCrawlerTest.java
@@ -0,0 +1,29 @@
+package com.esummary.crawler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InhaTcCrawlerTest {
+    private Crawler crawler = new InhaTcCrawler();
+
+    @Test
+    void crawlLoginSession() {
+    }
+
+    @Test
+    void crawlOwnCourse() {
+    }
+
+    @Test
+    void crawlLectureByWeek() {
+    }
+
+    @Test
+    void crawlAnnouncement() {
+    }
+
+    @Test
+    void crawlAssignment() {
+    }
+}
