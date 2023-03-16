e0dea4de3524f732706b66f056651ca47a3c8ff0
Jang_Desktop <sdsd090811@gmail.com>
2023-03-16 22:40:03 +0900

    Test: InhaTcCrawlerTest -  InhacrawlLoginSession 테스트 코드 추가

--- src/test/java/com/esummary/crawler/InhaTcCrawlerTest.java
+++ src/test/java/com/esummary/crawler/InhaTcCrawlerTest.java
@@ -1,6 +1,10 @@
 package com.esummary.crawler;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,7 +12,10 @@ class InhaTcCrawlerTest {
     private Crawler crawler = new InhaTcCrawler();
 
     @Test
+    @DisplayName("로그인 실패")
     void crawlLoginSession() {
+        Map<String, String> loginCookie = crawler.crawlLoginSession("201845096", "ABCDEFG");
+        Assertions.assertThat(loginCookie).isEqualTo(null);
     }
 
     @Test
