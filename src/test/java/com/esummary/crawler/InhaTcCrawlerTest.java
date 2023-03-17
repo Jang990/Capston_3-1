0a9ccfde0e252a4dedf0084e29888dad040b9e17
Jang_Desktop <sdsd090811@gmail.com>
2023-03-17 21:28:38 +0900

    Test: Optional로 수정

--- src/test/java/com/esummary/crawler/InhaTcCrawlerTest.java
+++ src/test/java/com/esummary/crawler/InhaTcCrawlerTest.java
@@ -1,21 +1,30 @@
 package com.esummary.crawler;
 
+import com.esummary.crawler.logincrawler.InhatcLoginCrawler;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InhaTcCrawlerTest {
-    private Crawler crawler = new InhaTcCrawler();
+    private Crawler crawler = new InhaTcCrawler(new InhatcLoginCrawler());
+
+    /*
+    @Autowired
+    private Crawler crawler;
+    */
 
     @Test
     @DisplayName("로그인 실패")
     void crawlLoginSession() {
-        Map<String, String> loginCookie = crawler.crawlLoginSession("201845096", "ABCDEFG");
-        Assertions.assertThat(loginCookie).isEqualTo(null);
+        Optional<Map<String, String>> loginCookie = crawler.crawlLoginSession("201845096", "1232132");
+        Assertions.assertThat(loginCookie.isEmpty()).isEqualTo(true);
     }
 
     @Test
