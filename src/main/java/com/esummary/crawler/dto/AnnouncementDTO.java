51003e63c5243a59642d31785f7ae1266977db28
Jang_Desktop <sdsd090811@gmail.com>
2023-03-19 16:13:54 +0900

    Feat: 공지 DTO에 필요한 정보들 추가

--- src/main/java/com/esummary/crawler/dto/AnnouncementDTO.java
+++ src/main/java/com/esummary/crawler/dto/AnnouncementDTO.java
@@ -1,5 +1,18 @@
 package com.esummary.crawler.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
 // 공지 정보
+@Builder
+@Getter
+@ToString
 public class AnnouncementDTO {
+    private String announcementId;
+    private String title;
+    private String author;
+    private String announcementDate;
+    private String content;
 }
+
