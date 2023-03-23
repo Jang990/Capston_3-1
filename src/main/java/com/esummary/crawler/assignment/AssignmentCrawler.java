069145c9940f7419c0f5c17736a171d62325ad20
Jang_Desktop <sdsd090811@gmail.com>
2023-03-23 16:59:59 +0900

    Feat: 과제 크롤러 인터페이스 및 구현체 추가
    일단 크롤링해서 과제 Element를 찍는 것까지 확인

---
+++ src/main/java/com/esummary/crawler/assignment/AssignmentCrawler.java
@@ -0,0 +1,15 @@
+package com.esummary.crawler.assignment;
+
+import com.esummary.crawler.dto.AnnouncementDTO;
+import com.esummary.crawler.dto.AssignmentDTO;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 세션ID 체크 같은 것들은 하지 않는다.
+ * 그냥 과제 크롤링만을 시도한다.
+ */
+public interface AssignmentCrawler {
+    List<AssignmentDTO> crawlAssignment(String courseId, Map<String, String> loginSessionCookie);
+}
