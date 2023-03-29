734ae76369028e86576f51e2c4aaf66d0271f65b
Jang_Desktop <sdsd090811@gmail.com>
2023-03-29 20:26:32 +0900

    Style: toString 추가

--- src/main/java/com/esummary/crawler/week/dto/LectureTime.java
+++ src/main/java/com/esummary/crawler/week/dto/LectureTime.java
@@ -3,11 +3,13 @@ package com.esummary.crawler.week.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalTime;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 public class LectureTime {
     private LocalTime totalTime;
