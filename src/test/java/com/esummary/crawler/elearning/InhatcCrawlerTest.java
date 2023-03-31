39227ded1dc5e780c8ba32c15199cde1fa6e8b9f
Jang_Desktop <sdsd090811@gmail.com>
2023-03-31 20:51:22 +0900

    Test: 메인 페이지 생성 쿠키 테스트 추가

--- src/test/java/com/esummary/crawler/elearning/InhatcCrawlerTest.java
+++ src/test/java/com/esummary/crawler/elearning/InhatcCrawlerTest.java
@@ -4,10 +4,14 @@ import com.esummary.crawler.elearning.announcement.InhatcAnnouncementCrawler;
 import com.esummary.crawler.elearning.login.exception.ExpiredELearningSession;
 import com.esummary.crawler.elearning.login.InhatcLoginCrawler;
 import com.esummary.crawler.elearning.login.LoginCrawler;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
@@ -58,6 +62,20 @@ class InhatcCrawlerTest {
     }
 
     @Test
-    void crawlAssignment() {
+    void crawlAssignment() throws IOException {
+        Map<String, String> cookies = Jsoup.connect("https://cyber.inhatc.ac.kr/MMain.do?cmd=viewIndexPage")
+                .method(Connection.Method.GET)
+                .execute().cookies();
+        System.out.println("cookies = " + cookies);
+
+        Connection conn = Jsoup.connect("https://cyber.inhatc.ac.kr/MUser.do")
+                .data("cmd", "loginUser")
+                .data("userDTO.userId", id)
+                .data("userDTO.password", password)
+                .data("userDTO.localeKey", "ko")
+                .cookies(cookies);
+
+        Document post = conn.post();
+        System.out.println("conn = " + post);
     }
 }
